6ff53b865d085f8bb979ae668560ade56f7db2b4
Nando Schär <schaer.nando@gmail.com>
2023-06-19 07:38:23 +0200

    fix: session prop on GetServerSessionPage

--- src/pages/next-auth-playgrounds/get-server-session.tsx
+++ src/pages/next-auth-playgrounds/get-server-session.tsx
@@ -4,7 +4,7 @@ import { getServerSession, Session } from 'next-auth';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 type GetServerSessionPageProps = {
-  session: Session | null;
+  serverSession: Session | null;
 };
 
 export default function GetServerSessionPage(
@@ -14,8 +14,8 @@ export default function GetServerSessionPage(
     <Layout>
       <div>
         <h1 className="text-3xl mb-3">Example with SSR and getServerSession</h1>
-        <div>isAuthenticated: {!!props.session ? 'true' : 'false'}</div>
-        <div>jwtPayload: {JSON.stringify(props.session)}</div>
+        <div>isAuthenticated: {!!props.serverSession ? 'true' : 'false'}</div>
+        <div>jwtPayload: {JSON.stringify(props.serverSession)}</div>
         <div className="mt-5">
           Mittels getServerSession() kann serverseitig geprüft werden, ob der Benutzer angemeldet ist oder nicht. Im
           Vergleich zu getToken() kann diese Methode ohne JWT Webtokens verwendet werden. Daher wird sie vor allem in
@@ -28,11 +28,10 @@ export default function GetServerSessionPage(
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getServerSession(req, res, authOptions);
-  console.warn('props', session);
 
   return {
     props: {
-      session,
+      serverSession: session,
     },
   };
 };
